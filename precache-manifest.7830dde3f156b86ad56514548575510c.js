0c593f828f3ad9e11f7601c2fe86d98901d46890
Mick Redman <guitarbeerchocolate@googlemail.com>
2020-03-18 15:22:12 +0000

    Updates

---
+++ precache-manifest.7830dde3f156b86ad56514548575510c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "819c267923552b3eab49fb5572b42f85",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7f8d3ab2abee72e7053",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b48caf98512384f90df8",
+    "url": "/static/js/2.78ac4bb3.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.78ac4bb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7f8d3ab2abee72e7053",
+    "url": "/static/js/main.e5dc360d.chunk.js"
+  },
+  {
+    "revision": "a53c56bcba93a6115910",
+    "url": "/static/js/runtime-main.4595a359.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
